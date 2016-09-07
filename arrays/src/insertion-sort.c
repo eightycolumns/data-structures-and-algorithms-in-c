39fe8cd9f95261ed7288a2be358c0b8987a325bb
Drew Keller <drew@eightycolumns.com>
2016-09-07 19:02:06 -0400

    Extract swap()

--- arrays/src/insertion-sort.c
+++ arrays/src/insertion-sort.c
@@ -1,11 +1,17 @@
 #include <stddef.h>
 
+static void swap(int *a, int *b);
+
 void insertion_sort(int *a, size_t n) {
   for (size_t i = 1; i < n; i += 1) {
     for (size_t j = i; j > 0 && a[j - 1] > a[j]; j -= 1) {
-      int temp = a[j - 1];
-      a[j - 1] = a[j];
-      a[j] = temp;
+      swap(&a[j - 1], &a[j]);
     }
   }
+}
+
+static void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
