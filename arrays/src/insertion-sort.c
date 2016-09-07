5a058d3e61cb7a3fd14fe963c09a3dbcf52a6e68
Drew Keller <drew@eightycolumns.com>
2016-09-07 19:04:47 -0400

    Simplify inner loop

--- arrays/src/insertion-sort.c
+++ arrays/src/insertion-sort.c
@@ -1,17 +1,16 @@
 #include <stddef.h>
 
-static void swap(int *a, int *b);
-
 void insertion_sort(int *a, size_t n) {
   for (size_t i = 1; i < n; i += 1) {
-    for (size_t j = i; j > 0 && a[j - 1] > a[j]; j -= 1) {
-      swap(&a[j - 1], &a[j]);
-    }
-  }
-}
+    int key = a[i];
 
-static void swap(int *a, int *b) {
-  int t = *a;
-  *a = *b;
-  *b = t;
+    size_t j = i;
+
+    while (j > 0 && a[j - 1] > key) {
+      a[j] = a[j - 1];
+      j -= 1;
+    }
+
+    a[j] = key;
+  }
 }
